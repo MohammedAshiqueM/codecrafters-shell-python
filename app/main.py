6e23c86afd1285798d64c261e1b4c7fed18cc06e
moh-ashique <mohammedashique942001@gmail.com>
2025-05-22 13:07:06 +0530

    type builtin

--- app/main.py
+++ app/main.py
@@ -7,15 +7,25 @@ def main():
     while(True):
         sys.stdout.write("$ ")
 
-        # Wait for user input
+        # built in commands
+        shell_builtin = {'echo','exit','type'}
+        #user input
         command = input()
-        first_word = command.split()[0]
+        command_list = command.split()
+        #checking the shell commads
         if command == 'exit 0':
             break
-        elif first_word == 'echo':
+        elif command_list[0] == 'echo':
             print(command[5:])
             continue
-        print(f'{command}: command not found')
+        elif command_list[0] == 'type':
+            if command_list[1] in shell_builtin:
+                print(f'{command_list[1]} is a shell builtin')
+            else:
+                print(f'{command_list[1]}: not found')
+            continue
+        else:
+            print(f'{command}: command not found')
 
 if __name__ == "__main__":
     main()
