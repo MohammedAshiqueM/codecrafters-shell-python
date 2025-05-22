1a4e6ee9cd11377a6789a15755b244d81df5c115
moh-ashique <mohammedashique942001@gmail.com>
2025-05-22 12:31:32 +0530

    echo builtin

--- app/main.py
+++ app/main.py
@@ -9,8 +9,12 @@ def main():
 
         # Wait for user input
         command = input()
+        first_word = command.split()[0]
         if command == 'exit 0':
             break
+        elif first_word == 'echo':
+            print(command[5:])
+            continue
         print(f'{command}: command not found')
 
 if __name__ == "__main__":
